d38ff0fff42080e2e7dbe8d15c19f6913ea30dff
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-05-19 09:51:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+
+function second_highest(Arr) {
+    let highest_num = Arr[0];
+    let second_highest_num = -Infinity;
+    
+    // Find the highest number in the array
+    for (let i = 1; i < Arr.length; i++) {
+      if (Arr[i] > highest_num) {
+        second_highest_num = highest_num;
+        highest_num = Arr[i];
+      } else if (Arr[i] > second_highest_num && Arr[i] < highest_num) {
+        second_highest_num = Arr[i];
+      }
+    }
+    
+    return second_highest_num;
+  }
+  
+  const Arr = [1,2,3,4,5,6,7,8,9,10];
+  console.log(second_highest(Arr));
+
